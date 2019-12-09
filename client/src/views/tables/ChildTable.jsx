97c05d0c2d297f8f2499b80abaf298f199360c76
Caren B <carenpbriones@gmail.com>
2019-12-09 00:23:40 -0800

    Fixed broken links on About page

--- client/src/views/tables/ChildTable.jsx
+++ client/src/views/tables/ChildTable.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 
 // reactstrap components
@@ -24,9 +25,8 @@ class ChildTable extends React.Component {
     API.getUser(this.props.user.id).then((res) => {
       this.setState({
         children: res.data.children
-      })
-      console.log(this.state.children);
-    })
+      });
+    });
   }
 
   showChildren(arr) {
@@ -38,54 +38,54 @@ class ChildTable extends React.Component {
             <CardText>Please add a child, to access session tracking tools.  </CardText>
             <Button href="/admin/addChild" className=" btn-primary">
               Add A Child
-                      </Button>
+            </Button>
           </CardBody>
         </Card>
-      )
+      );
 
     } else {
 
       return (
         <div className="content">
-        <Card>
-          <CardHeader>
-            <h5 className="title float-left">Children</h5>
-            <Button href="/admin/addChild" color="info" size="sm" className="float-right">
+          <Card>
+            <CardHeader>
+              <h5 className="title float-left">Children</h5>
+              <Button href="/admin/addChild" color="info" size="sm" className="float-right">
               Add Child
-                      </Button>
-          </CardHeader>
-          <CardBody>
-            <Table responsive>
-              <thead>
-                <tr>
-                  <th>Child Name</th>
-                  <th>D.O.B</th>
-                  <th>Gender</th>
-                  <th>Therapist</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.children.map((child) =>
-                  <tr key={child._id}>
-                    <td><a href={`child/${child._id}`}>{child.firstName} {child.lastName}</a></td>
-                    <td>{child.dateOfBirth.slice(0, 10)}</td>
-                    <td>{child.gender}</td>
-                    <td>{child.therapist}</td>
-                    <td>
-                      <Button href={`newSession/${child._id}`} color="success" size="sm">
-                        Start Session
-                      </Button>
-                    </td>
+              </Button>
+            </CardHeader>
+            <CardBody>
+              <Table responsive>
+                <thead>
+                  <tr>
+                    <th>Child Name</th>
+                    <th>D.O.B</th>
+                    <th>Gender</th>
+                    <th>Therapist</th>
+                    <th>Actions</th>
                   </tr>
-                )}
-              </tbody>
-            </Table>
-          </CardBody>
-        </Card>
+                </thead>
+                <tbody>
+                  {this.state.children.map((child) =>
+                    <tr key={child._id}>
+                      <td><a href={`child/${child._id}`}>{child.firstName} {child.lastName}</a></td>
+                      <td>{child.dateOfBirth.slice(0, 10)}</td>
+                      <td>{child.gender}</td>
+                      <td>{child.therapist}</td>
+                      <td>
+                        <Button href={`newSession/${child._id}`} color="success" size="sm">
+                        Start Session
+                        </Button>
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </Table>
+            </CardBody>
+          </Card>
         </div>
 
-      )
+      );
 
 
     }
@@ -135,9 +135,9 @@ class ChildTable extends React.Component {
       </Card> */}
 
         {this.showChildren(this.state.children)}
-        </div>
+      </div>
       </>
-    )
+    );
   }
 
 }
